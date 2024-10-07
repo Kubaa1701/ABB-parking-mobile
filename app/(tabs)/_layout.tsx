3be8012005fe6a0ef1d9da827c25d1cb6be8ac3f
Kubaa1701 <kubapaciorek66@gmail.com>
2024-10-07 10:30:07 +0200

    add basic navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,36 @@
+import { Tabs } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import React from 'react';
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'green' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome
+              size={28}
+              name="home"
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome
+              size={28}
+              name="gear"
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
