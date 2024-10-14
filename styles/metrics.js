53ccced14e04bb4f82ba82a74413d5c2df4ade07
szymurmur <szymo0215@gmail.com>
2024-10-14 12:29:21 +0200

    add functions for calculating metrics

---
+++ styles/metrics.js
@@ -0,0 +1,12 @@
+import { Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+const guidelineBaseWidth = 375;
+const guidelineBaseHeight = 812;
+
+const horizontalScale = (size) => (width / guidelineBaseWidth) * size;
+const verticalScale = (size) => (height / guidelineBaseHeight) * size;
+const moderateScale = (size) => size + (horizontalScale(size) - size) * 0.5;
+
+export { horizontalScale, verticalScale, moderateScale };
