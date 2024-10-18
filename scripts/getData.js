3774ff142646cc739e89e25da7d9cc147e529e96
Kubaa1701 <kubapaciorek66@gmail.com>
2024-10-18 08:18:26 +0200

    change url

--- scripts/getData.js
+++ scripts/getData.js
@@ -1,5 +1,7 @@
 export default async function getData(endpoint) {
-  const url = 'http://localhost:5000/' + endpoint;
+  const url =
+    'https://parking-api-f6gjdyg6hpa9hjh0.polandcentral-01.azurewebsites.net/' +
+    endpoint;
   try {
     const response = await fetch(url);
     data = await response.json();
